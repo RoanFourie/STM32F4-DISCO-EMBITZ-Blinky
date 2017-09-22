e941b3bec71392845325b48e62e41f7468c3f898
Roan Fourie <roan.fourie@gmail.com>
2017-09-22 14:16:18 +0200

    Add push button

--- STM32F407Disco/src/main.c
+++ STM32F407Disco/src/main.c
@@ -14,8 +14,25 @@
 #include "stm32f4xx_conf.h"
 
 #include "stm32f4xx.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
 #include "fm_stm32f4_led.h"
 #include "fm_stm32f4_delay.h"
+static void button_setup(void)
+{
+
+
+	// Clock Enable
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+    // Config as digital output
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
 
 int main(void)
 {
@@ -25,14 +42,21 @@ int main(void)
     delay_init();
     // all LED initialize
     FM_Led_Init();
-  while(1)
-  {
-    // Toggle all LED
-       FM_Led_Toggle(LED_GREEN);
-       FM_Led_Toggle(LED_ORANGE);
-       FM_Led_Toggle(LED_RED);
-       FM_Led_Toggle(LED_BLUE);
-       // Delay for 1sec
+
+    //volatile int i;
+    while (1) {
+        if (gpio_get(GPIOA, GPIO_Pin_0)) {
+            FM_Led_On(LED_GREEN);
+            FM_Led_On(LED_BLUE);
+            FM_Led_Off(LED_ORANGE);
+            FM_Led_Off(LED_RED);
+        } else {
+            FM_Led_Off(LED_GREEN);
+            FM_Led_Off(LED_BLUE);
+            FM_Led_On(LED_ORANGE);
+            FM_Led_On(LED_RED);
+        }
+        // Delay for 1sec
        delay_ms(1000);
-  }
+    }
 }
